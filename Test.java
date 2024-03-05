48f8410e13f4b4d3882cb06bb7242140dacb4316
MarineAvramova <avramovamarine@gmail.com>
2024-03-05 10:16:19 +0100

    Update Test.java
    Deleated @BeforeEach, @AfterEach methodes

--- Test.java
+++ Test.java
@@ -4,16 +4,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
 class Test {
-
-	@BeforeEach
-	void setUp() throws Exception {
-	}
-
-	@AfterEach
-	void tearDown() throws Exception {
-	}
-
-	@org.junit.jupiter.api.Test
+        @org.junit.jupiter.api.Test
 	void shouldArrayIndexOutOfBoundsExceptionIsThrown() {
 		CalcularDNI calcularDNI = new CalcularDNI();
 		
